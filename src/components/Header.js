d5b77a077a7f489c537db047b399dbee38d3dc68
Wasif Raza <mwasifraza95@gmail.com>
2022-09-24 20:40:55 +0500

    header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,23 +24,35 @@ const Header = () => {
   return (
     <>
     <div className='container'>
-        <div className='row header'>
-            <div className='logo'>
-                <h2 className='text-primary p-3 mx-5'>GPA CALCULATOR</h2>
-            </div>
-            <div className='offset-md-2 col-md-8'>
-                <div className='box my-4'>
-                    <h1>What's your name?</h1>
-                    <div className='name-field'>
-                        <input type="text" name="fullname" className='form-control' onKeyUp={() => {validateName()}} placeholder='John Doe, etc' />
-                        <span className='icon check hide'><i className="fas fa-check-circle"></i></span>
-                        <span className='icon times hide'><i className="fas fa-times-circle"></i></span>
+        <div className='row align-items-center header'>
+            <div className='header-child'>
+                <nav className='navbar'>
+                    <a className="navbar-brand" href="#">
+                        <img src="image/logo.svg" alt="logo" width={50} />
+                        &nbsp;&nbsp;GPA Calculator
+                    </a>
+                </nav>
+                <div className='header-body p-2'>
+                    <div className='box'>
+                        <h1>What's your name?</h1>
+                        <div className='name-field'>
+                            <input type="text" name="fullname" className='form-control' onKeyUp={() => {validateName()}} placeholder='John Doe, etc' />
+                            <span className='icon check hide'><i className="fas fa-check-circle"></i></span>
+                            <span className='icon times hide'><i className="fas fa-times-circle"></i></span>
+                        </div>
+                    </div>
+                    <button className='btn btn-outline-primary px-5 mt-5' type="button" disabled>Continue</button>
+                </div>
+                <div className='footer py-2 px-3'>
+                    <ul className='header-socials'>
+                        <li><a href="#"><i className="fab fa-facebook"></i></a></li>
+                        <li><a href="#"><i className="fab fa-linkedin"></i></a></li>
+                        <li><a href="#"><i className="fab fa-github"></i></a></li>
+                    </ul>
+                    <div className='d-flex align-items-center'>
+                        <h6><i className='far fa-copyright'></i> 2022 | Designed by <a href='#'>Wasif Raza</a></h6>
                     </div>
                 </div>
-                <button className='btn btn-outline-primary px-5' type="button" disabled>Continue</button>
-            </div>
-            <div className='credits'>
-                <h6><i className='far fa-copyright'></i> 2022 | Designed by <a href='#' className='text-primary'>Wasif Raza</a></h6>
             </div>
         </div>
     </div>
